ba551f4ed0d29c8b15914ecf782cae6b88386003
kleitonalbuquerque <kleiton2102@gmail.com>
2023-06-15 19:39:39 -0300

    Primeiro commit

---
+++ 1_intro/index.js
@@ -0,0 +1,9 @@
+var firstName = "Kleiton";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(firstName);
+// greeting(anotherName);
+// greeting(x);
